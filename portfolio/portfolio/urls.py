d1f4bf8c9baad4f6d1e90abaaccceb29b512cef9
lvaka <mpstudiosEJShin@gmail.com>
2018-02-13 21:59:32 -0800

    ignore

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('folio.urls')),
-]
+] 
